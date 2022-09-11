89fd1a6958b4263e5ecdf92fcc90bec9b325347b
Fansy <418536538@qq.com>
2022-09-12 01:13:10 +0800

    feat: 绑定`Pre`的样式

--- src/components/Renderer/Pre.tsx
+++ src/components/Renderer/Pre.tsx
@@ -1,15 +1,19 @@
 import { defineComponent } from 'vue';
 import highlight from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
+import { useRendererContext } from './context';
 
 highlight.registerLanguage('javascript', javascript);
 
 const Pre = defineComponent({
     name: 'PreRenderer',
     setup(props, { slots }) {
+        const { createNamespace } = useRendererContext();
+
         return () => {
             return (
                 <pre
+                    class={createNamespace('pre')}
                     innerHTML={
                         highlight.highlight((slots.default?.()[0].children as string) || '', { language: 'javascript' })
                             .value
