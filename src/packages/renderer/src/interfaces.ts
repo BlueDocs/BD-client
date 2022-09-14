18f36c14c2d9a908976ba8796909fc148b35efc0
Fansy <35361626+fanhaoyuan@users.noreply.github.com>
2022-09-14 21:22:17 +0800

    feat: Renderer 组件添加 attrs 属性 (#13)

--- src/packages/renderer/src/interfaces.ts
+++ src/packages/renderer/src/interfaces.ts
@@ -1,15 +1,23 @@
 import { VNode } from 'vue';
 
+export type MarkdownRendererComponentAttrs = Record<string, string>;
+
+export type MarkdownRendererComponent = (attrs: MarkdownRendererComponentAttrs) => VNode;
+
+export type MarkdownRendererComponents = Record<string, MarkdownRendererComponent>;
+
 export type MarkdownTokenType = 'element' | 'text' | 'code_block';
 
 export interface MarkdownToken {
     type: MarkdownTokenType;
     children?: MarkdownToken[];
+    attrs?: MarkdownRendererComponentAttrs;
 }
 
 export interface MarkdownElement extends MarkdownToken {
     tag: string;
     type: 'element';
+    attrs: MarkdownRendererComponentAttrs;
 }
 
 export interface MarkdownText extends MarkdownToken {
@@ -21,10 +29,5 @@ export interface MarkdownCodeBlock extends MarkdownToken {
     tag: 'pre';
     content: string;
     type: 'code_block';
+    attrs: MarkdownRendererComponentAttrs;
 }
-
-export type MarkdownRendererComponentAttrs = Record<string, any>;
-
-export type MarkdownRendererComponent = (attrs?: MarkdownRendererComponentAttrs) => VNode;
-
-export type MarkdownRendererComponents = Record<string, MarkdownRendererComponent>;
